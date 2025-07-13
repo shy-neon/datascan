7dc4e3f30b8def14118aef582e897b2188534507
Nicola Di Gregorio <nicoladigregorio@192.168.1.57>
2025-07-13 21:42:29 +0200

    ciao

--- src/js/heromap.js
+++ src/js/heromap.js
@@ -1,52 +1,52 @@
 
 require([
-      "esri/WebMap",
-      "esri/views/MapView",
-      "esri/widgets/FeatureTable",
-      "esri/core/reactiveUtils",
-      "esri/layers/support/ImageElement"
-    ], function(WebMap, MapView, FeatureTable, reactiveUtils, ImageElement) {
+  "esri/WebMap",
+  "esri/views/MapView",
+  "esri/widgets/FeatureTable",
+  "esri/core/reactiveUtils",
+  "esri/layers/support/ImageElement"
+], function (WebMap, MapView, FeatureTable, reactiveUtils, ImageElement) {
 
-      var webmap = new WebMap({
-        portalItem: {
-          id: "07696c5b72e64125ae75f65226471f60"  // Inserisci qui l’ID della tua WebMap
-        }
-      });
+  var webmap = new WebMap({
+    portalItem: {
+      id: "07696c5b72e64125ae75f65226471f60"  // Inserisci qui l’ID della tua WebMap
+    }
+  });
 
-      var view = new MapView({
-        container: "viewDiv",
-        map: webmap,
-        constraints: {
-          minZoom: 4,         
-          maxZoom: 4
-        },
-       
-        
-      });
+  var view = new MapView({
+    container: "viewDiv",
+    map: webmap,
+    constraints: {
+      minZoom: 4,
+      maxZoom: 4
+    },
 
-    
 
-      
-      view.ui.remove("zoom");
-      view.ui.remove("OUTLINE");
+  });
 
-      view.on("mouse-wheel", (event) => event.stopPropagation());
-      view.on("drag", (event) => event.stopPropagation());
-      view.on("double-click", (event) => event.stopPropagation());
-      view.on("double-click", ["Control"], (event) => event.stopPropagation());
 
-      view.when(function() {
-        view.goTo(
-            {
-                center: [-7.48, 41.74],
-                zoom: 10
-            },
-            {
-                duration: 4000 // 4 secondi
-            }
-        );
-    });
-    })
-   
 
-     
+
+  view.ui.remove("zoom");
+  view.ui.remove("OUTLINE");
+
+  view.on("mouse-wheel", (event) => event.stopPropagation());
+  view.on("click", (event) => event.stopPropagation());
+  view.on("drag", (event) => event.stopPropagation());
+  view.on("double-click", (event) => event.stopPropagation());
+  view.on("double-click", ["Control"], (event) => event.stopPropagation());
+
+  view.when(function () {
+    view.goTo(
+      {
+        center: [-7.48, 41.74],
+        zoom: 10
+      },
+      {
+        duration: 4000 // 4 secondi
+      }
+    );
+  });
+})
+
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,32 @@
 var amount = document.getElementById('count');
 
-
+let profs = document.getElementsByClassName('prof')
 var current = 0;
-var current1 = 0;
-
 
 update();
 
 function update() {
     amount.innerText = current.toLocaleString("en-US");
-  
 }
 
-function update1() {
-    
-}
 
-setInterval(function(){
-    if(current < 3440000){
+setInterval(function () {
+    if (current < 3440000) {
         current = current + 4523
     }
     update();
-},0.0001);
+}, 0.0001);
+
+console.log(profs)
+
+for (let i = 0; i < profs.length; i++) {
+    profs[i].addEventListener("click", function () {
+        // Rimuove "selected" da tutti
+        for (let j = 0; j < profs.length; j++) {
+            profs[j].classList.remove("selected");
+        }
+        // Aggiunge "selected" solo a quello cliccato
+        this.classList.add("selected");
+    });
+}
 
